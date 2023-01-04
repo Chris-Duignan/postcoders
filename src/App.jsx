1168394e64dcd99fbbc1e8177d0c2cd520d8d260
Chris-Duignan <cduignan.1993@gmail.com>
2023-01-04 14:07:10 +0000

    allow outcode entry through input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,54 @@
-import { useEffect, useState } from 'react'
-import { getAreaData } from './api'
+import { useEffect, useState } from "react";
+import { getAreaData } from "./api";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   const [areas, setAreas] = useState([]);
+  const [search, setSearch] = useState("");
+  const [outcode, setOutcode] = useState("bb10");
 
   const load = async () => {
     try {
-      const areaData = await getAreaData()
+      const areaData = await getAreaData(outcode);
 
       setAreas(areaData);
     } catch (error) {
-      window.alert("todo: fix app")
+      window.alert("todo: fix app");
     }
-  }
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setOutcode(search);
+  };
 
   useEffect(() => {
     load();
-  }, []);
+  }, [outcode]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter the outcode:{" "}
+          <input
+            type="text"
+            value={search}
+            name="search"
+            onChange={handleChange}
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <h2>{`Areas for ${outcode}: ${areas.length}`}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
